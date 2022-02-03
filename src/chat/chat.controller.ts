2dbb1108466a3a30d64ad088e49b16fff534230e
Jo-wonbin <jwb7214@gmail.com>
2022-02-04 01:05:11 +0900

    Feat : 채팅 모듈 추가

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,10 +1,23 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { ChatService } from './chat.service';
+import { MessageDto } from './dto/message.dto';
 
-@Controller('chat')
+@Controller('api/chat')
 export class ChatController {
   constructor(private chatService: ChatService) {}
 
   @Post()
-  postChat() {}
+  postChat(
+    @Param('roomId') roomId: string,
+    @Param('senderId', ParseIntPipe) senderId: bigint,
+    @Param('sentTime') sentTime: string,
+    @Param('content') content: string,
+  ) {
+    return this.chatService.postChat({
+      roomId,
+      senderId,
+      sentTime,
+      content,
+    });
+  }
 }
--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ChattingHistory } from '../entities/ChattingHistory';
+import { Repository } from 'typeorm';
+import { EventsGateway } from '../events/events.gateway';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+  constructor(
+    @InjectRepository(ChattingHistory)
+    private chattingHistoryRepository: Repository<ChattingHistory>,
+    private eventsGateway: EventsGateway,
+  ) {}
+
+  async postChat({ roomId, senderId, sentTime, content }) {
+    const chatLog = new ChattingHistory();
+    chatLog.roomId = roomId;
+    chatLog.senderId = senderId;
+    chatLog.content = content;
+    chatLog.sentTime = sentTime;
+
+    const savedChat = await this.chattingHistoryRepository.save(chatLog);
+    if (!savedChat) {
+      throw new NotFoundException('저장에 실패하였습니다.');
+    }
+
+    // const arr = roomId.split('-', 2);
+    // if (senderId !== parseInt(arr[0])) {
+    //   const receiverId = parseInt(arr[0]);
+    // } else {
+    //   const receiverId = parseInt(arr[1]);
+    // }
+    this.eventsGateway.server.to(`/${roomId}`).emit('message', savedChat);
+  }
+}
